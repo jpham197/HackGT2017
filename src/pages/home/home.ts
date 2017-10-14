2556b336e411441b206d039e7b9313a632d45471
kashyap@codemettle.com <kashyap@codemettle.com>
2017-10-14 14:13:03 -0400

    Updated homepage with searchbar and list

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,40 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+	items;
 
   constructor(public navCtrl: NavController) {
+  	this.initializeItems();
+  }
 
+  initializeItems() {
+  	this.items = [
+  		'Nintendo Switch',
+  		'PS4',
+  		'Xbox',
+  		'iPhone X',
+  		'Water bottle',
+  		'Shirt',
+  		'Sunglasses',
+  		'Free swag',
+  		'Starbucks coffee',
+  		'Internship'
+  	];
+  }
+
+  getItems(ev) {
+  	this.initializeItems();
+  	var val = ev.target.value;
+
+  	if (val && val.trim() != '') {
+  		this.items = this.items.filter((item) => {
+  			return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+  		})
+  	}
+  }
+
+  goToItemPage() {
+  	this.navCtrl.push('FirstPage');
   }
 
 }
