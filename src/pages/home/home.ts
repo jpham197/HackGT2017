f1ee86fe3da71e2a36362f4d5e475165897e94de
jpham197 <jpham197@gmail.com>
2017-10-14 23:26:56 -0400

    Turned showSplashScreen false in config and display splash through css animation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,24 +1,53 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 import { NcrapiProvider } from '../../providers/ncrapi/ncrapi';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public nrcService: NcrapiProvider) {
+  // constructor(public navCtrl: NavController, public nrcService: NcrapiProvider) {
+	//
+  // }
+	splash = true;
+
+
+	constructor(public toastCtrl: ToastController) {
 
   }
 
-  getdata() {
-    this.nrcService.getdata().subscribe(res => {
-      console.log(res);
-    });
-  }
+	ionViewDidLoad() {
+		setTimeout(() => {
+			this.splash = false;
+		}, 4000);
+	}
 
-  gotoFirstPage() {
-    this.navCtrl.push('FirstpagePage');
-  }
+	// constructor(public splashScreen: splashScreen) {
+	//
+	// }
+
+  // getdata() {
+  //   this.nrcService.getdata().subscribe(res => {
+  //     console.log(res);
+  //   });
+  // }
+	//
+  // gotoFirstPage() {
+  //   this.navCtrl.push('FirstpagePage');
+  // }
+
+	//fix
+	showToast() {
+		let toast = this.toastCtrl.create({
+			message: "Toast is good",
+			duration: 1000
+		});
+		toast.present();
+	}
+
+	// this.splashScreen.show();
 }
